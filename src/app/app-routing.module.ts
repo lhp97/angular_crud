42eb96f6ad8c3a36e33451fb013108b859a9cf38
lhp97 <lhdprado@gmail.com>
2022-04-15 14:56:45 -0300

    add roteamento lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  { path: 'courses',
+    loadChildren: () => import('./courses/courses-routing.module').then(m => m.CoursesRoutingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
